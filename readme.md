6e70193630e2f8389bfad800d85756faf4435e9c
Tossou Cyriaque <cyriaque.tossou@etudiant.univ-rennes1.fr>
2024-03-21 07:06:32 +0100

    docker ok, starting adding swagger

---
+++ readme.md
@@ -0,0 +1,2 @@
+# usefull video for docker config with spring boot and mysql
+https://www.youtube.com/watch?v=-ekBqIvAGY4
--- src/main/java/fr/istic/science/ScienceInnovPartyApplication.java
+++ src/main/java/fr/istic/science/ScienceInnovPartyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ScienceInnovPartyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScienceInnovPartyApplication.class, args);
+		SpringApplication.run(ScienceInnovPartyApplication.class, args); System.out.println("Application démarrée avec succès !");
 	}
 
 }
